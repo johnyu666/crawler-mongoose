bca66a60984cc122a18a694a87a3d67e1c3442fc
yuyinghan <johnyucn@163.com>
2020-01-11 15:24:03 +0800

    app.js中的bug修复

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let storer=require('./service/Storer')
 let crawler=require('./service/Crawler')
 
-crawler.fetch("http://johnyu.cn:3000/albums",function (albums) {
+crawler.fetch("http://johnyu.cn:3000/albums",function (err,albums) {
+    if(!err)
      storer.addAlbums(albums,function (err,newAlbums) {
          if(!err) console.log(newAlbums)
      })
+    else console.log(err)
 })
